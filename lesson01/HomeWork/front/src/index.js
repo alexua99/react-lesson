f98de437a7a1820eb2576349353b6eabc86e865c
AlexUa99 <2356931k@gmail.com>
2023-02-11 13:46:23 +0200

    cvbc

---
+++ lesson01/HomeWork/front/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <p>
+    1: Зробити Header footer <br/>
+    2: Секцію > картинка + кнопка
+    3: використовувати scss
+    </p> */}
+  </React.StrictMode>
+);
+
+
